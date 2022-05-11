c36bc231766b48159a2024a9d8d584aa1b79450a
July-GX <gx009@outlook.com>
2022-05-11 20:13:58 +0800

    修改

--- js/init.js
+++ js/init.js
@@ -515,7 +515,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Java工程师", "UI/UX 设计师", "Web 开发人员","游戏热爱者","优秀资源共享者"],
+			strings: ["Java工程师", "UI/UX 设计师", "Web 开发人员","游戏热爱者"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
